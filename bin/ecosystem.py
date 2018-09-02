7a6249fe872fc1bc2b8fcfe3fdd88f9c7d5dc963
Gaetan Guidet <gatgui@gmail.com>
2018-09-02 16:57:47 +0900

    bump version to 0.8.0

--- bin/ecosystem.py
+++ bin/ecosystem.py
@@ -1302,7 +1302,7 @@ def main(argv=None):
 
     # parse the (command line) arguments; python 2.7+ (or download argparse)
     import argparse
-    description = 'Peregrine Ecosystem, environment, build and deploy management toolset v0.7.0'
+    description = 'Peregrine Ecosystem, environment, build and deploy management toolset v0.8.0'
     parser = argparse.ArgumentParser(prog='ecosystem',
                                      formatter_class=argparse.RawTextHelpFormatter,
                                      description=description,
